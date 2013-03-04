94cb17177b923c3043c1568dd7ea2da376aafc45
Sophia van Valkenburg <sophiavv@bu.edu>
2013-03-03 21:57:07 -0500

    changed size of legend in plots

--- plot-data/sv_binary_real.m
+++ plot-data/sv_binary_real.m
@@ -4,7 +4,8 @@ Yr = dlmread(real_file)
 Xb = [1 : length(Yb)]'
 Xr = [1 : length(Yr)]'
 fig = figure
+set(gcf,'PaperPositionMode','auto')
 plot(Xb,Yb,'b',Xr,Yr,'r','LineWidth',2.0)
 leg = legend('binary-valued','real-valued','Location','East')
-set(leg,'FontSize',20)
+set(leg,'FontSize',50)
 print(fig, output_file,'-dpng')
